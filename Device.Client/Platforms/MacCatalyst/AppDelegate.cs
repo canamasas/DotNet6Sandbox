ff95766905adb3761a137e1d4f4ced4b07dec707
Bruno Canamasas <bkx7481@varian.com>
2022-01-27 11:49:15 +0200

    Play with file upload

---
+++ Device.Client/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Device.Client;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
